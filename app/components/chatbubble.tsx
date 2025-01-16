86d8e9585c747494c7de68adb40218afcccd0456
Michael <ch.michael085@gmail.com>
2025-01-15 16:44:40 -0800

    Chat bubble

---
+++ app/components/chatbubble.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as motion from "motion/react-client"
+
+const ChatBubble = () => {
+    return (
+        <motion.div className="inline-flex items-center bg-[#FFFFFF] rounded-2xl px-4 py-3">
+            <div className="flex gap-1.5">
+                {[0, 1, 2].map((i) => (
+                    <motion.div
+                        key={i}
+                        animate={{ y: [0, -3, 0] }}
+                        transition={{
+                            duration: 1,
+                            repeat: Infinity,
+                            ease: "easeInOut",
+                            delay: i * 0.25
+                        }}
+                        className="w-1.5 h-1.5 bg-[#BAC2C9] rounded-full"
+                    />
+                ))}
+            </div>
+        </motion.div>
+    );
+};
+
+export default ChatBubble;
