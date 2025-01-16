4a59844958db8f10f1d9e6c0bb1e9e32be2fb5f6
Michael <ch.michael085@gmail.com>
2025-01-15 16:44:40 -0800

    Chat bubble

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 import * as motion from "motion/react-client"
+import ChatBubble from "./components/chatbubble"
 
 export default function Home() {
   return (
     <main className="overflow-hidden h-full w-full">
       <div className="flex flex-col mt-24 sm:mt-40 justify-center items-center text-center gap-2 text-neutral-600 relative">
-        <motion.p
+        <motion.div
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.6, ease: "easeOut" }}
-          className="w-52 sm:w-96 font-sans text-lg leading-6 mt-28">
+          className="w-52 sm:w-96 font-sans text-lg leading-6 mt-28"
+        >
           is a
-          <span className="mx-10">
-            ...
+          <span className="mx-3">
+            <ChatBubble />
           </span>
-          who is learning web dev, game dev, ui/ux,
-          and other methods of creating experiences that
-          make a lasting impression.
-        </motion.p>
+          <p>
+            currently learning web dev, game dev, ui/ux, and other methods of
+            creating experiences that leave meaningful impressions.
+          </p>
+        </motion.div>
       </div>
     </main>
   );
