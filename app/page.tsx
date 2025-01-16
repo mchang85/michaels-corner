ffa2ac9e575719a40d66143e167a79b1c12b87bd
Michael <ch.michael085@gmail.com>
2025-01-15 18:24:07 -0800

    Font family

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,25 @@ import ChatBubble from "./components/chatbubble"
 export default function Home() {
   return (
     <main className="overflow-hidden h-full w-full">
-      <div className="flex flex-col mt-24 sm:mt-40 justify-center items-center text-center gap-2 text-neutral-600 relative">
-        <motion.p
+      <div className="flex flex-col mt-24 sm:mt-40 justify-center items-center text-center text-gray-500 relative">
+        <motion.div
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.6, ease: "easeOut" }}
-          className="w-52 sm:w-96 font-sans text-lg leading-6 mt-28">
+          className="w-52 sm:w-96 font-sans text-xl leading-6 mt-28"
+        >
           is a
           <span className="mx-3">
             <ChatBubble />
           </span>
-          who is learning web dev, game dev, ui/ux,
-          and other methods of creating experiences that
-          make a lasting impression.
-        </motion.p>
+          <p>
+            currently learning web dev, game dev, ui/ux, and other methods of
+            creating experiences that leave meaningful impressions.
+          </p>
+        </motion.div>
+      </div>
+      <div className="flex justify-end w-full absolute bottom-4 text-gray-500 font-mono">
+        <p className="text-sm mr-4">coded with <span className="text-red-500">&lt;3</span></p>
       </div>
     </main>
   );
