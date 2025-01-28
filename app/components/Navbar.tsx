efc18f94e98766e41460e752e91c52d1d390270c
Michael <ch.michael085@gmail.com>
2025-01-27 17:41:24 -0800

    fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Link from "next/link";
-import Image from "next/image";
 import { usePathname } from "next/navigation";
 import * as motion from "motion/react-client"
 import { useEffect, useState } from "react";
